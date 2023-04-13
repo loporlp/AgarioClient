d374116ffe1514ec4f7ce7122c71224e24d2bc63
Mason Sansom <masonsansom02@gmail.com>
2023-04-13 11:25:46 -0600

    Removed random comments

--- ClientGUI/WorldDrawable.cs
+++ ClientGUI/WorldDrawable.cs
@@ -9,29 +9,27 @@ namespace ClientGUI
         private readonly float screenHeight;
         private readonly float screenWidth;
 
-        public WorldDrawable(World world) 
+        public WorldDrawable(World world)
         {
             this.world = world;
             screenHeight = 500;
             screenWidth = 500;
         }
-      
+
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
             canvas.FillColor = Colors.DarkBlue;
             canvas.FillRectangle(0, 0, screenHeight, screenWidth);
 
-         //   canvas.FillCircle(model.x, model.y, 10);
-            
             int foodDrawn = 0;
-            foreach(Food food in world.food.Values)
+            foreach (Food food in world.food.Values)
             {
                 canvas.FillColor = Color.FromInt(food.ARGBColor);
                 Vector2 pos = getScreenPosition(food.X, food.Y);
                 canvas.FillCircle(pos.X, pos.Y, food.Radius);
                 foodDrawn++;
             }
-            
+
         }
 
         public Vector2 getScreenPosition(float X, float Y)
